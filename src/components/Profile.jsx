3c0972e3968be4299cebea3fc9f9e86714c3ec9e
Hussain Thajutheen <hussainibnthaj@gmail.com>
2021-06-20 23:13:28 +0530

    feat: add profile component

---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+import { AUTH_TOKEN, getUserSnippet } from "../helper";
+
+const Profile = () => {
+  const user = JSON.parse(localStorage.getItem(AUTH_TOKEN));
+  console.log("user", user);
+  const history = useHistory();
+
+  return (
+    <div className="container">
+      <div className="profile-container">
+        <div className="user-snippet">
+          <h1>{getUserSnippet(user.user.name)}</h1>
+        </div>
+        <h2>{user.user.name}</h2>
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            localStorage.removeItem(AUTH_TOKEN);
+            history.push("/");
+          }}
+        >
+          Log Out
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
